9b88cf61cfd56ae332e1561ba2c1683ebd649e34
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-19 22:36:10 +0800

    add getCityServiceDetail api

--- ucar-java-ride/src/test/java/com/github/zhangchunsheng/ucarride/service/BaseTest.java
+++ ucar-java-ride/src/test/java/com/github/zhangchunsheng/ucarride/service/BaseTest.java
@@ -1,7 +1,7 @@
 package com.github.zhangchunsheng.ucarride.service;
 
 public class BaseTest {
-    public final static String ACCESS_TOKEN = "06342a58-1c90-4ac4-acc7-b62bb83188cd";
+    public final static String ACCESS_TOKEN = "3220985e-7c75-4f34-824e-8ca423d170b9";
     protected final static int CITY = 1;
     protected final static int SERVICE = 14;
 
